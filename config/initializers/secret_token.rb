60863027c1c087801a224e1553f654af5684f566
Norman Tran <normantran86@gmail.com>
2014-01-22 20:56:26 -0600

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gamematch::Application.config.secret_key_base = '5de0ca4a033e9e26516cb481e5cffaa0d02a31a368f19ec82af0b6407f34c73316d184a0ab11434c2896846c8ccd79af8387b2da46402aa0135e9087161855bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gamematch::Application.config.session_store :cookie_store, key: '_gamematch_session'
